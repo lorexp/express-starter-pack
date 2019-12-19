a0e43da1611fa69bb10380853826512b16e45ff5
lorexp <vinixp.vp@gmail.com>
2019-12-18 22:16:38 -0300

    fix typo

--- index.js
+++ index.js
@@ -5,38 +5,42 @@ const { exec } = require("child_process");
 const data =
   "const express = require('express');\n\napp = express();\n\nconst port = 3000;\n\n\napp.listen(port, () => {console.log(`Listening on port ${port}`)});";
 
-fs.mkdir(path.resolve("./config"), () => {
-  console.log("Creating config folder");
-});
+function createApp() {
+  fs.mkdir(path.resolve("./config"), () => {
+    console.log("Creating config folder");
+  });
 
-fs.mkdir(path.resolve("./routes"), () => {
-  console.log("Creating routes folder");
-});
+  fs.mkdir(path.resolve("./routes"), () => {
+    console.log("Creating routes folder");
+  });
 
-fs.mkdir(path.resolve("./controllers"), () => {
-  console.log("Creating controllers folder");
-});
+  fs.mkdir(path.resolve("./controllers"), () => {
+    console.log("Creating controllers folder");
+  });
 
-fs.mkdir(path.resolve("./models"), () => {
-  console.log("Creating models folder");
-});
+  fs.mkdir(path.resolve("./models"), () => {
+    console.log("Creating models folder");
+  });
 
-fs.writeFile(path.resolve(__dirname + "/file.js"), data, () => {
-  console.log("Created file.js");
-});
+  fs.writeFile(path.resolve(__dirname + "/file.js"), data, () => {
+    console.log("Created file.js");
+  });
 
-exec("npm init -y", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  }
+  exec("npm init -y", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+    }
 
-  console.log("Created package.json");
-});
+    console.log("Created package.json");
+  });
 
-exec("npm i express", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  }
+  exec("npm i express", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+    }
 
-  console.log("Installed express");
-});
+    console.log("Installed express");
+  });
+}
+
+module.exports = createApp;
