dbe3fae6582cc60927462777bda1d8b329b88ed3
lorexp <vinixp.vp@gmail.com>
2019-12-19 07:36:20 -0300

    changed to cli.js

---
+++ cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+
+const data =
+  "const express = require('express');\n\napp = express();\n\nconst port = 3000;\n\n\napp.listen(port, () => {console.log(`Listening on port ${port}`)});";
+
+fs.mkdir(path.resolve("./config"), () => {
+  console.log("Creating config folder");
+});
+
+fs.mkdir(path.resolve("./routes"), () => {
+  console.log("Creating routes folder");
+});
+
+fs.mkdir(path.resolve("./controllers"), () => {
+  console.log("Creating controllers folder");
+});
+
+fs.mkdir(path.resolve("./models"), () => {
+  console.log("Creating models folder");
+});
+
+fs.writeFile(path.resolve("./index.js"), data, () => {
+  console.log("Created file.js");
+});
+
+exec("npm init -y", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+  }
+
+  console.log("Created package.json");
+});
+
+exec("npm i express", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+  }
+
+  console.log("Installed express");
+});
